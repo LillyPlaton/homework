356d69f88fea1345041cb78ff17c6c4fac3676df
Liliana Platon <l.platon@web.de>
2023-11-20 16:59:33 +0100

    main

--- src/homework30/Main02.java
+++ src/homework30/Main02.java
@@ -2,4 +2,12 @@ package homework30;
 
 public class Main02 {
 
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 3;
+    double c = 2;
+    double d = 7;
+    System.out.println(a * (b-d) + c * a / d);
+  }
+
 }
